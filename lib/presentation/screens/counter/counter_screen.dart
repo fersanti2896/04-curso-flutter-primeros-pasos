5a0526c5aa3865466523a2b8db8a924c4cd1a749
Fernando Nicolás <fersanti2896@gmail.com>
2023-05-16 02:33:34 -0600

    Diseño de contador

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -6,7 +6,20 @@ class CounterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(child: Text('¡Counter Screen!'))
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('10',
+                style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100)),
+            Text('Clicks', style: TextStyle(fontSize: 25))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        child: Icon(Icons.plus_one),
+      ),
     );
   }
 }
